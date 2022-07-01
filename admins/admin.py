d80fbbcbd663a23627216765f39bc484192a1354
Varsha Shaheen <varshashaheen2003@gmail.com>
2022-07-01 12:47:15 +0530

    admin

--- admins/admin.py
+++ admins/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from admins.models import Announcements, Crop, Weather
+from admins.models import Announcements, Crop, Weather, Homepage
 
 admin.site.register(Announcements)
 admin.site.register(Weather)
 admin.site.register(Crop)
+admin.site.register(Homepage)
